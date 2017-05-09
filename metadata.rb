49d8a2f0d1a4cf626b2d8423a3dfd4da53f3dde5
Carlos Ordoyo <carlos.ordoyo@example.es>
2017-05-09 18:55:15 +0000

    Initial commit

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'workstation'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Carlos Ordoyo'
+maintainer_email 'carlos@example.com'
 license 'all_rights'
 description 'Installs/Configures workstation'
 long_description 'Installs/Configures workstation'
